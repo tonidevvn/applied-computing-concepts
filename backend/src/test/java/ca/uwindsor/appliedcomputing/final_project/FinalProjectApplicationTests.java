38a2fd75c8eebbf5a315fe29bbae5e6454776604
hungnp <hungnp1@ghn.vn>
2024-06-20 16:27:11 -0400

    initial commit

---
+++ backend/src/test/java/ca/uwindsor/appliedcomputing/final_project/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.uwindsor.appliedcomputing.final_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
