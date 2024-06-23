71fdf0ee5af577aabba9020a249abee7fe338df1
chuong <chuongtat@gmail.com>
2024-06-22 23:33:41 -0400

    remove an unused exception

--- backend/src/main/java/ca/uwindsor/appliedcomputing/final_project/controller/SpellCheckingController.java
+++ backend/src/main/java/ca/uwindsor/appliedcomputing/final_project/controller/SpellCheckingController.java
@@ -2,7 +2,6 @@ package ca.uwindsor.appliedcomputing.final_project.controller;
 
 import ca.uwindsor.appliedcomputing.final_project.dto.DistanceEntry;
 import ca.uwindsor.appliedcomputing.final_project.service.SpellCheckingService;
-import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +16,6 @@ public class SpellCheckingController {
     @Autowired
     private SpellCheckingService spellCheckingService;
 
-    @SneakyThrows
     @GetMapping
     public List<String> getWordCompleteList(@RequestParam("word") String text) {
         List<DistanceEntry> lDE = spellCheckingService.spellChecking(text);
